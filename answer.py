24182efaa41105100f07edc8a28b80509d1c21ba
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-09-18 20:53:18 +0100

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,2 +1,5 @@
 def array(string):
     return None if len(string) < 3 or len(string.split(","))<3  else " ".join(string.split(",")[1:len(string.split(","))-1])
+
+def array(strng):
+    return ' '.join(strng.split(',')[1:-1]) or None
